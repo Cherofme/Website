75ecbe2854d3b520d96745ec9414867b8c1d27ba
Cherofme <denisderivedmed7@gmail.com>
2023-01-18 10:23:36 +0200

    new venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Andrew\Python\project-gymsite\venv
+set VIRTUAL_ENV=C:\Users\denis\OneDrive\Документи\GitHub\Website\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
